ced3ba69b8f4adba9f178881723574d3511da629
sunbin <csb7386@gmail.com>
2023-11-13 23:42:20 +0900

    [ADD] SpringBoot start

---
+++ src/main/java/com/server/muchu/MuchuApplication.java
@@ -0,0 +1,13 @@
+package com.server.muchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuchuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuchuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/muchu/MuchuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.muchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuchuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
