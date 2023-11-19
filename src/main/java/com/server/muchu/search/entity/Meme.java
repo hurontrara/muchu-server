a45afe0edb1728b12bb74854b4745c9cba6e16d5
sunbin <csb7386@gmail.com>
2023-11-19 19:31:20 +0900

    [Feat] 밈 엔티티 추가
    - 영어로 짤 -> "Meme"
    - 기본적으로 pk, 파일명, 링크(S3) 컬럼
    - 현재는 군집번호 정도만 추가
    - 이후 사용할 비지도 학습 알고리즘에 따라 추가 예정

---
+++ src/main/java/com/server/muchu/search/entity/Meme.java
@@ -0,0 +1,38 @@
+package com.server.muchu.search.entity;
+
+import com.server.muchu.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+// 엔티티에는, NoArgsConstructor 가 필수적
+// Builder 패턴에서, 생성자를 정의하게 되면 전체 생성자 또한 정의해야함
+// '다른 생성자가 존재하지 않을 때' 전체 생성자를 자동으로 만들어주므로
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "MEME_TB")
+public class Meme extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEME_ID")
+    private Long id;
+
+    @Column(name = "MEME_FILENAME", nullable = false)
+    private String fileName;
+
+    @Column(name = "MEME_CLUSTER")
+    private int clusterNum;
+
+    @Column(name = "MEME_LINK", nullable = false)
+    private String link;
+
+
+}
