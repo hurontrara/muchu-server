ba47e2c5938c939f55149085585085cc6b6546db
sunbin <csb7386@gmail.com>
2023-11-27 12:21:01 +0900

    [Feat] 컨트롤러 계층 추가
    - NAVER, KAKAO 로그인 지원

---
+++ src/main/java/com/server/muchu/security/oauth/controller/OAuthController.java
@@ -0,0 +1,45 @@
+package com.server.muchu.security.oauth.controller;
+
+
+import com.server.muchu.security.oauth.params.KakaoLoginParams;
+import com.server.muchu.security.oauth.params.NaverLoginParams;
+import com.server.muchu.security.oauth.service.OAuthLoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+@RequestMapping("/oauth2/code")
+@RequiredArgsConstructor
+public class OAuthController {
+
+    private final OAuthLoginService oAuthLoginService;
+
+    @GetMapping("/naver")
+    public void naverCodeResponse(NaverLoginParams params, HttpServletResponse response) throws IOException {
+
+        oAuthLoginService.login(params, response);
+
+        response.sendRedirect("/");
+
+
+    }
+
+    @PostMapping("/kakao")
+    public void kakaoCodeResponse(KakaoLoginParams params, HttpServletResponse response) throws IOException {
+
+        oAuthLoginService.login(params, response);
+
+        response.sendRedirect("/");
+
+
+    }
+
+}
